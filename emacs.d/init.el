36d8463a59280b641cfdcd18e566f017dfcf5d3e
Yun Sheng <uewing@gmail.com>
2016-08-29 08:53:52 -0400

    no theme for emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -135,15 +135,3 @@ Return a list of installed packages or nil for every skipped package."
 ;; (toggle-scroll-bar -1)
 ;; (menu-bar-mode -1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
